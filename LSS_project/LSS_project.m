bc837b24fa0d62dc4ed213205ffd5250f8f6c4a7
Sebastian Bobadilla Suarez <sebastian.suarez.12@ucl.ac.uk>
2018-04-06 11:37:58 +0100

    minor modifications in paths

--- LSS_project/LSS_project.m
+++ LSS_project/LSS_project.m
@@ -13,18 +13,18 @@ clear all; close all; clc;
 % by Sebastian Bobadilla Suarez
 
 cd ..
-pd = pwd; %parent directory for rsatoolbox
 
 %cd /home/seb/Documents/rsatoolbox/
-toolboxRoot = pwd; addpath(genpath(toolboxRoot));
+toolboxRoot = pwd;  %parent directory for rsatoolbox
+addpath(genpath(toolboxRoot)); %add all sub-paths to path
 
 % addpath to the libsvm toolbox in case it's not in your matlab path
 addpath('/home/seb/Documents/libsvm-3.22/matlab');
 
 %% setup model parameters
 
-parjob = 0; %1 if using cluster, runs njobs
-njobs = 50; %number of jobs per noise setting, only if running on many cores
+parjob = 1; %1 if using cluster, runs njobs
+njobs = 500; %number of jobs per noise setting, only if running on many cores
 
 simulationOptions.nRepititions = 20; %4; %repetitions per stimulus
 simulationOptions.nruns = 3; %separate fMRI blocks
@@ -35,7 +35,7 @@ simulationOptions.nConditions = 2; %how many stimulus conditions (i.e., classes)
 
 simulationOptions.svm_options = '-s 0 -t 0 -n 0.5 -h 0'; %nu-svm is supposed to be -s 1??? -t 0 is linear kernel, -t 2 rbf, -h 0 is faster (shrinking heuristic)
 % check libsvm website for more information on options
-simulationOptions.nf = 100; %number of features (voxels) that go into the classifier
+simulationOptions.nf = 300; %number of features (voxels) that go into the classifier
 
 all_models = 1;
 if all_models==0
@@ -45,8 +45,8 @@ else
 end
 
 simulationOptions.trial_sigma = 0.5^2; %variance between trials for each stimulus condition
-simulationOptions.exp = 1; %modify exponent, higher means more like identity matrix
-simulationOptions.volumeSize_vox = [5 5 5]; % size of the signal
+simulationOptions.exp = 1; %modify exponent, higher means more like identity matrix for the covariance matrix input to a wishart distribution
+simulationOptions.volumeSize_vox = [7 7 7]; % size of the signal
 simulationOptions.signal_voxels = prod(simulationOptions.volumeSize_vox); % number of signal voxels
 simulationOptions.cov_mat_df = simulationOptions.signal_voxels^simulationOptions.exp; % modify exponent, higher means more like identity matrix
 simulationOptions.corrs = 0.7^2; %correlations for the covariance matrix
@@ -71,8 +71,8 @@ simulationOptions.effectCen = [20 20 15];
 
 % with these two lists, 8 levels will be run in total
 
-trial_duration_list = [2,3,4,5]; % in seconds
-big_sigma_list = [10, 12, 14, 16]; %[2,3,4,5]; % sigma for the hyper-ellipse containing the mean random vectors
+trial_duration_list = [2,3,4,5]; % in seconds %2
+big_sigma_list = [10, 15];%[20, 30, 40, 50]; %[2,3,4,5]; % sigma for the hyper-ellipse containing the mean random vectors
 %scan_noise_list = [3000, 5000, 7000, 10000]; %if needed you can add a for
 %loop below with different levels of scanner noise
 
@@ -90,7 +90,7 @@ for trial_duration = 1:length(trial_duration_list)
             disp(simulationOptions.model_list)
             disp(accs)
             
-            cd(pd)
+            cd(toolboxRoot)
             %save(['sig_',num2str(simulationOptions.big_sigma),'_isi_', num2str(simulationOptions.trialDuration),'.mat'],'accs')
         else
             simulationOptions.trialDuration = trial_duration_list(trial_duration);
@@ -112,15 +112,15 @@ for trial_duration = 1:length(trial_duration_list)
             disp(simulationOptions.model_list)
             disp(mean(all_accs))
             
-            if exist([pd '/LSS_project/sim_results/'],'dir')==0
-                mkdir([pd '/LSS_project/sim_results/'])
+            if exist([toolboxRoot '/LSS_project/sim_results/'],'dir')==0
+                mkdir([toolboxRoot '/LSS_project/sim_results/'])
             end
             
-            cd([pd '/LSS_project/sim_results/'])
-            save(['bsig',num2str(simulationOptions.big_sigma),'tsig',num2str(trial_sigma),'isi', num2str(simulationOptions.trialDuration),'.mat'],'SIMS','all_accs')
-            %save(['scan_sig',num2str(simulationOptions.scannerNoiseLevel),'tsig',num2str(trial_sigma),'isi', num2str(simulationOptions.trialDuration),'.mat'],'SIMS','all_accs')
+            cd([toolboxRoot '/LSS_project/sim_results/'])
+            save(['bsig',num2str(simulationOptions.big_sigma),'tsig',num2str(simulationOptions.trial_sigma),'isi', num2str(simulationOptions.trialDuration),'.mat'],'SIMS','all_accs')
+            %save(['scan_sig',num2str(simulationOptions.scannerNoiseLevel),'tsig',num2str(simulationOptions.trial_sigma),'isi', num2str(simulationOptions.trialDuration),'.mat'],'SIMS','all_accs')
             
-            cd([pd '/LSS_project/'])
+            cd([toolboxRoot '/LSS_project/'])
         end
         
     end
